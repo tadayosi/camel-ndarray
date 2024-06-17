e083b5688c0f836ef2f9de5c45c51af1caa90565
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-06-17 17:43:22 +0900

    wip

--- src/main/java/org/apache/camel/NdArrayDataFormat.java
+++ src/main/java/org/apache/camel/NdArrayDataFormat.java
@@ -1,26 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.spi.DataFormat;
 import org.apache.camel.spi.DataFormatName;
+import org.apache.camel.spi.annotations.Dataformat;
 import org.apache.camel.support.service.ServiceSupport;
 
-@org.apache.camel.spi.annotations.Dataformat("ndarray")
+@Dataformat("ndarray")
 public class NdArrayDataFormat extends ServiceSupport implements DataFormat, DataFormatName {
+
+    public NdArrayDataFormat() {
+    }
 
     public String getDataFormatName() {
         return "ndarray";
     }
 
+    @Override
     public void marshal(Exchange exchange, Object graph, OutputStream stream) throws Exception {
         // marshal from the Java object (graph) to the ndarray type
         byte[] bytes = exchange.getContext().getTypeConverter().mandatoryConvertTo(byte[].class, graph);
         stream.write(bytes);
     }
 
+    @Override
     public Object unmarshal(Exchange exchange, InputStream stream) throws Exception {
         // unmarshal from the input stream of ndarray to Java object (graph)
         byte[] bytes = exchange.getContext().getTypeConverter().mandatoryConvertTo(byte[].class, stream);
@@ -28,13 +49,13 @@ public class NdArrayDataFormat extends ServiceSupport implements DataFormat, Dat
     }
 
     @Override
-    protected void doStart() throws Exception {
-        // init logic here
+    protected void doStart() {
+        // noop
     }
 
     @Override
-    protected void doStop() throws Exception {
-        // cleanup logic here
+    protected void doStop() {
+        // noop
     }
 
 }
